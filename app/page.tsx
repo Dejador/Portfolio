f33270715eee4985be5d0d6b8fda986440b0a94e
Adolfo Murillo <adolfomv@gmail.com>
2023-06-21 15:56:14 -0600

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,157 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { skills, projects } from '../common/data';
+
+function Navbar() {
+  return (
+    <div className='max-w-[1200px] w-full bg-cyan-950 h-16 fixed flex items-center justify-between px-5 border-l-[3px] border-r-[3px] border-amber-400 drop-shadow-[0_10px_35px_rgba(255,255,255,0.50)]'>
+      <div className='w-12 h-full'>
+        <a href='#' className='w-12 mt-2 h-12 absolute'></a>
+        <div className='flex h-12 mt-2'>
+          <Image
+            src='https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/portfolio_pic.png?alt=media&token=9036f7b5-a995-469a-aaa2-f07d4a8fda78'
+            width={500}
+            height={500}
+            alt='Profile Pic'
+          />
+        </div>
+      </div>
+      <div className='flex gap-3'>
+        <a href='#projects' className=' hover:text-amber-400'>
+          Projects
+        </a>
+        <a href='#skills' className=' hover:text-amber-400'>
+          Skills
+        </a>
+        <a
+          href='mailto:adolfomv@gmail.com'
+          className=' animate-pulse hover:text-amber-400 hover:animate-none'
+        >
+          Let's Talk
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function Personal() {
+  return (
+    <>
+      <div className='text-2xl lg:text-5xl text-center flex'>
+        <p className='text-amber-400'>Welcome</p>, I'm Adolfo Murillo
+      </div>
+      <div className='w-3/4 lg:w-1/2 text-center mt-6'>
+        <div>
+          A Frontend Web Developer in the making with an extensive background in
+          design and customer success who loves to engage in new challenges.
+          <br />
+          <br />
+          I'm building my portfolio and focusing on Job opportunities where I
+          can contribute, learn and grow.
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Skills({ skills: [] }) {
+  return (
+    <>
+      <div className='text-2xl text-amber-400 mb-12'>Skills</div>
+      <div className='flex flex-wrap justify-center text-center gap-12 w-3/4 bg-white text-black rounded-3xl py-5 px-3'>
+        {skills.map(({ name, imageUrl, imageAlt }, index) => (
+          <div
+            key={index}
+            className='w-[25px] md:w-[50px] lg:w-[50px] flex flex-col items-center'
+          >
+            <Image
+              className='rounded-md'
+              src={imageUrl}
+              width={500}
+              height={500}
+              alt={imageAlt}
+            />
+            <div className='text-xs mt-2'>{name}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Projects({ projects: [] }) {
+  return (
+    <>
+      <div className='text-2xl text-amber-400 mb-12'>Projects</div>
+      {projects.map(
+        (
+          { name, description, imageUrl, imageAlt, websiteUrl, githubUrl },
+          index
+        ) => (
+          <div
+            key={index}
+            className='flex-col flex items-center text-center w-3/4'
+          >
+            <div className='bg-zinc-500 items-center w-full flex flex-col py-6 px-6 rounded-3xl mb-6'>
+              <div className='text-xl mb-4 border-b-2 border-amber-400 px-2'>
+                {name}
+              </div>
+              <div className='mb-4'>{description}</div>
+              <div className='flex-col lg:flex lg:flex-row gap-16'>
+                <Image
+                  className='rounded-md'
+                  src={imageUrl}
+                  width={500}
+                  height={500}
+                  alt={imageAlt}
+                />
+                <div className='flex flex-col gap-2 items-center mt-4 lg:m-auto'>
+                  <a
+                    className='px-1 w-32 leading-3 py-2 text-sm bg-neutral-800 rounded-md border border-amber-400 hover:scale-105'
+                    href={websiteUrl}
+                    target='_blank'
+                  >
+                    Visit Website
+                  </a>
+                  <a
+                    className='px-1 w-32 leading-3 py-2 text-sm bg-neutral-800 rounded-md border border-amber-400 hover:scale-105'
+                    href={githubUrl}
+                    target='_blank'
+                  >
+                    View on Github
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      )}
+    </>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <>
+      <div className='flex-col flex w-full items-center'>
+        <div className='mt-28' />
+        <Navbar />
+        <div className='flex flex-col w-full items-center'>
+          <Personal />
+        </div>
+        <div id='skills' className='py-12' />
+        <Skills skills={[]} />
+        <div id='projects' className='py-12' />
+        <Projects projects={[]} />
+        <div />
+        <div className='mt-4 mb-6'>
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='mailto:adolfomv@gmail.com'
+            className='text-cyan-500 hover:text-cyan-600'
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            adolfomv@gmail.com
           </a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
---
+++ common/data.js
@@ -0,0 +1,79 @@
+export const skills = [
+  {
+    name: 'HTML',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/logo_html.svg?alt=media&token=fccb80e9-f5ae-462b-85e2-2b2a2ac1e7d9',
+    imageAlt: 'HTML Logo',
+  },
+  {
+    name: 'CSS',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/logo_css.svg?alt=media&token=dad63ae3-4259-4735-ae17-5ee035ec4dfc',
+    imageAlt: 'CSS Logo',
+  },
+  {
+    name: 'JavaScript',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/logo_js.svg?alt=media&token=9dc689cd-35f0-4c9f-b0e2-c81b6dbdc72b',
+    imageAlt: 'JavaScript Logo',
+  },
+  {
+    name: 'React',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/logo_react.svg?alt=media&token=b9f8c902-d688-4fe7-ae0f-f4ef2dc92d34',
+    imageAlt: 'React Logo',
+  },
+  {
+    name: 'Next.js',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/logo_nextjs.svg?alt=media&token=ff0207f5-7111-4aee-8a38-74aea567bd87',
+    imageAlt: 'Next.js Logo',
+  },
+  {
+    name: 'Vercel',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/logo_vercel.svg?alt=media&token=3cfd0439-21d7-44cc-b830-f071319796bc',
+    imageAlt: 'Vercel Logo',
+  },
+  {
+    name: 'Tailwind',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/logo_tailwind.svg?alt=media&token=455664f1-624f-4acf-99c9-ec7e91adc495',
+    imageAlt: 'Tailwind Logo',
+  },
+  {
+    name: 'Github',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/logo_github.svg?alt=media&token=82656644-05b4-4ad5-8727-ee20c96d0c4b',
+    imageAlt: 'Github Logo',
+  },
+  {
+    name: 'Adobe Suite',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/logo_adobe.svg?alt=media&token=0d0507f1-2758-4b24-b3c5-205f3f942332',
+    imageAlt: 'Adobe Suite Logo',
+  },
+];
+
+export const projects = [
+  {
+    name: 'B-Fit',
+    description:
+      'A personal project dedicated to fitness, build implementing API calls, authentication and full CRUD and using technologies such as React, Next.js, RapidAPI, Firebase and Tailwind.',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/BeFit_01_B.gif?alt=media&token=3afdcc29-5c39-4734-86af-6d488bc69766',
+    imageAlt: 'B-Fit Gif',
+    websiteUrl: 'https://b-fit.vercel.app/',
+    githubUrl: 'https://github.com/Dejador/B-Fit',
+  },
+  {
+    name: 'Tic-Tac-Toe!',
+    description:
+      'A fun project recreating the classic Tic-Tac-Toe game using React and CSS.',
+    imageUrl:
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-91e68.appspot.com/o/tictactoe.gif?alt=media&token=9f6850f1-547f-4e0f-8cce-04f12fd0600a',
+    imageAlt: 'TicTacToe Gif',
+    websiteUrl: 'https://tictactoe-dejador.vercel.app/',
+    githubUrl: 'https://github.com/Dejador/tictactoe',
+  },
+];
